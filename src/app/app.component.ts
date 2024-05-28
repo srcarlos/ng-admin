f94ca667af46ff8e1ff8c56bdd1d861f58b1326f
Carlos Marcano <carlos.marcano@samsistemas.com.ar>
2024-05-27 22:59:58 -0300

    merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterModule, RouterOutlet, Routes } from '@angular/router';
+import { first, switchMap } from 'rxjs';
+
+import { CommonModule } from '@angular/common';
+import { Product } from './_helpers/data/products';
+import { ProductService } from './components/common/dummy/product/product.service';
+import { CounterService } from './components/common/dummy/product/counter.service';
+import { SubService } from './components/common/dummy/product/sub.service';
+import { LayoutComponent } from './components/common/core/layouts/layout/layout.component';
+import { ToolbarComponent } from './components/common/core/layouts/layout/toolbar/toolbar.component';
+
+import { AccountStatusPartialComponent } from './components/private/dashboard/status.partial';
+import { AuthState } from './_services/auth/auth.state';
+import { AccountService } from './_services/auth/account.service';
+import { AuthService } from './_services/auth/auth.service';
+
+import { LoginFormComponent } from './components/public/login-form/login-form.component';
+import { SignupFormComponent } from './components/public/signup-form/signup-form.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [
+    LayoutComponent,
+    ToolbarComponent,
+    CommonModule,
+    RouterOutlet,
+    AccountStatusPartialComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    RouterModule,
+  ],
+  providers: [ProductService, CounterService, SubService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-admin';
+  filterText: string = 'XL';
+  @Input() product: Product | undefined;
+  @Input() counter: number | undefined;
+  productTest: Product | undefined;
+  //subscription: Subscription;
+
+  constructor(
+    private subService: SubService,
+    private authState: AuthState,
+    private accountService: AccountService,
+    private authService: AuthService
+  ) {
+    this.subService.getSub().subscribe((ProductDetails) => {
+      console.log('Component A', ProductDetails);
+      this.product = ProductDetails;
+    });
+
+    // in here, tap into authstate to watch when it becomes available,
+    // and request more info from account
+    // do it just once through out a session
+    this.authState.stateItem$
+      .pipe(
+        first((state) => state !== null),
+        switchMap((state) => this.accountService.GetAccount())
+      )
+      .subscribe({
+        next: (response) => {
+          console.log('recieved', response);
+
+          // here you should set account state if you have it
+          // then you can use it elsewhere
+          // this.accountState.SetState(response);
+        },
+      });
+  }
+
+  addItem(newItem: Product) {
+    this.productTest = newItem;
+  }
+
+  onSelected(product: Product) {
+    this.product = product;
+  }
+  onClick() {}
+
+  ngOnInit(): void {}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,58 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+import {
+  BrowserModule,
+  bootstrapApplication,
+  provideClientHydration,
+} from '@angular/platform-browser';
+import { AppRoutingModule } from './app/routes/app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { AuthState } from './app/_services/auth/auth.state';
+import { AppInterceptor } from './app/_helpers/http.interceptor';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './app/material/material.module';
+
+// add a provider to array of providers
+const CoreProviders = [
+  {
+    provide: APP_INITIALIZER,
+    // dummy factory
+    useFactory: () => () => {},
+    multi: true,
+    // injected depdencies, this will be constructed immidiately
+    deps: [AuthState],
+  },
+  // you might want to add a configuration service
+  // add interceptors
+  {
+    provide: HTTP_INTERCEPTORS,
+    multi: true,
+    useClass: AppInterceptor,
+  },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      HttpClientModule,
+      FormsModule,
+      ReactiveFormsModule,
+      MaterialModule
+    ),
+    provideClientHydration(),
+    ...CoreProviders,
+  ],
+}).catch((err) => console.error(err));
